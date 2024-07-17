2c5faf6f5221b472c4e081a04d2cae92f91d8205
Mangesh05012001 <108254403+Mangesh05012001@users.noreply.github.com>
2024-07-17 09:09:09 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict";
+//tredt all JS code newer version
+
+
+
+console.log(3
+    +
+    3);
+// code readability should be high
+
+
+console.log("Hitesh");
+
+let name="mangesh"
+let age=24
+let isLoggedIn=false
+let state;
+// number => 2 to power
+// bigint
+// string=>""
+// boolean=> true/false
+// null =>standalone value
+// undefined=>
+    // symbol => unique
+//object
+
+
+console.log(typeof "Mangesh");
+console.log( typeof undefined);//undefined
+console.log( typeof null);//null
+
