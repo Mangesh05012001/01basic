a8025127304bc6188ec7a11a8bfe85c88c636a76
Mangesh05012001 <108254403+Mangesh05012001@users.noreply.github.com>
2024-07-17 09:12:12 +0000

    variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,8 +2,14 @@ const accountId =123456
 let accountEmail="mharal@gmail.com"
 var accountPassword="1234"
 accountCity="pune"
+let accountState;
 
-
+// accountId=123459876    Not allowed
+accountEmail="haral@gmail.com"
+accountPassword="7899876"
+accountCity="nashik"
 
 
 console.log(accountId);
+
+console.table([accountId, accountEmail,accountPassword,accountCity,accountState]);
