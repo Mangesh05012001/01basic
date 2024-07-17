ca07c509d7b40055f68af5e4cf7033a8fd7b5841
Mangesh05012001 <108254403+Mangesh05012001@users.noreply.github.com>
2024-07-17 15:10:11 +0000

    conversion Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,29 +1,64 @@
-let score ="33abc"
-console.log(typeof score);
-console.log(typeof(score));
+// let score ="33abc"
+// console.log(typeof score);
+// console.log(typeof(score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
-
-
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
-//"33"=> 33
-// "33abc"=> NaN
-//true=> 1; false=>0
 
-let isLoggedIn=1
 
-let booleanIsLoggedIn =Boolean(isLoggedIn)
+// //"33"=> 33
+// // "33abc"=> NaN
+// //true=> 1; false=>0
 
-console.log(booleanIsLoggedIn);
+// let isLoggedIn=1
 
-// 1 =>true ; 0 => false
-//"" =>false
-//"hitesh"=>true 
+// let booleanIsLoggedIn =Boolean(isLoggedIn)
 
-let someNumber = 33 
-let stringNumber =String(someNumber)
-console.log(stringNumber);//33
-console.log(typeof stringNumber);
+// console.log(booleanIsLoggedIn);
+
+// // 1 =>true ; 0 => false
+// //"" =>false
+// //"hitesh"=>true 
+
+// let someNumber = 33 
+// let stringNumber =String(someNumber)
+// console.log(stringNumber);//33
+// console.log(typeof stringNumber);
+
+//**********************Operations ************************
+// let value = 3
+//  let negValue = -value
+//  console.log(negValue);
+
+
+
+//  console.log(2+2);
+//  console.log(2-2);
+//  console.log(2*2);
+//  console.log(2**2);
+//  console.log(2/2);
+//  console.log(2%2);
+
+ 
+//  let str1 = "hello"
+//  let str2="Mangesh"
+//  let str3=str1+str2
+//  console.log(str3);
+
+//  console.log("1"+2);
+//  console.log(1+"2");
+//  console.log(1 + 2 + "2");
+ 
+// console.log((3 + 4 )* 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gameCounter=100
+-- gameCounter 
+console.log(gameCounter);
+
